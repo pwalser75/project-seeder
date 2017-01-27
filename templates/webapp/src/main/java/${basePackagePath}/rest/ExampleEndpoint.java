94bdc80736db623033294828edd5cd0f9c523e7e
Peter Walser <pwalser@frostnova.ch>
2017-01-27 23:12:52 +0100

    Fixed imports

--- templates/webapp/src/main/java/${basePackagePath}/rest/ExampleEndpoint.java
+++ templates/webapp/src/main/java/${basePackagePath}/rest/ExampleEndpoint.java
@@ -1,21 +1,12 @@
 package ${basePackage}.rest;
 
         import javax.enterprise.context.RequestScoped;
-        import javax.json.Json;
-        import javax.json.JsonObjectBuilder;
-        import javax.json.JsonStructure;
-        import javax.json.JsonWriter;
         import javax.ws.rs.GET;
         import javax.ws.rs.Path;
         import javax.ws.rs.Produces;
         import javax.ws.rs.core.MediaType;
-        import java.io.StringWriter;
         import java.text.Collator;
-        import java.util.Collections;
-        import java.util.LinkedList;
-        import java.util.List;
-        import java.util.Properties;
-
+        import java.util.*;
 
 /**
  * Example JAX-RS rest web service
