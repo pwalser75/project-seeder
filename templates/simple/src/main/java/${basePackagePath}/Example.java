d35fa65ac237661580162edb7e45df56e0f464bf
Peter Walser <peter.walser@ti8m.ch>
2017-06-14 09:31:58 +0200

    Added logging configuration

--- templates/simple/src/main/java/${basePackagePath}/Example.java
+++ templates/simple/src/main/java/${basePackagePath}/Example.java
@@ -1,13 +1,22 @@
 package ${basePackage};
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Example class
  */
 public class Example {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public static void main(String[] args) {
-        Example example = new Example();
-        System.out.println(example.getMessage());
+        new Example();
+    }
+
+    public Example() {
+
+        log.info(getMessage());
     }
 
     public String getMessage() {
